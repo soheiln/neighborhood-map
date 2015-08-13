b86bd5ef7b572dd809ea3b335036c11f4e178dfb
Soheil Norouzi <soheil.nrz@gmail.com>
2015-08-13 12:29:36 -0700

    Code refactoring

--- js/app.js
+++ js/app.js
@@ -1,65 +1,76 @@
 //GLOBAL VARIABLES
 
 var GOOGLE_MAP_API_KEY = 'AIzaSyB76u_0lqPeUPSWCyH0Lr5zp7GExa5Rc_Q';
-
 var map,
   infowindow,
-  myViewModel;
+  myViewModel,
+  sfLocation = {
+    lat: 37.7833,
+    lng: -122.4167
+  };
 
-var sfLocation = {
-  lat: 37.7833,
-  lng: -122.4167
-};
 
-
+//Callback function run by maps once loaded
 function initMap() {
+
   map = new google.maps.Map(document.getElementById('map'), {
     center: sfLocation,
     zoom: 14
   });
 
-  infowindow = new google.maps.InfoWindow();
+  searchPlaces();
+}
 
+
+// Searches places on the map around center and for each place
+// creates a marker and adds it to list view
+//TODO: add functionality for search around center
+var searchPlaces = function() {
+  infowindow = new google.maps.InfoWindow();
   var service = new google.maps.places.PlacesService(map);
 
-  console.log('before service nearbySearch');
   service.nearbySearch({
     location: sfLocation,
     radius: 500,
     types: ['store']
   }, placesCallback);
-
-  function placesCallback(results, status) {
-    console.log('in placesCallback');
-    if (status === google.maps.places.PlacesServiceStatus.OK) {
-      for (var i = 0; i < results.length; i++) {
-        console.dir(results[i]);
-        createMarker(results[i]);
-      }
-    }
-  };
-
-  function createMarker(place) {
-    var placeLoc = place.geometry.location;
-    var marker = new google.maps.Marker({
-      map: map,
-      position: place.geometry.location
-    });
-
-    google.maps.event.addListener(marker, 'click', function() {
-      infowindow.setContent(place.name);
-      infowindow.open(map, this);
-      myViewModel.setCurrentMarker(place);
-    });
-  }
-
 }
+
+// callBack function that handles the response from Google PlacesService API
+function placesCallback(results, status) {
+  console.log('in placesCallback');
+  if (status === google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      console.dir(results[i]);
+      createMarker(results[i]);
+    }
+  }
+};
+
+
+// Takes a place object as input, creates the corresponding maker on map
+// and adds click listeners to it
+function createMarker(place) {
+  var placeLoc = place.geometry.location;
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location
+  });
+
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.setContent(place.name);
+    infowindow.open(map, this);
+    myViewModel.setCurrentMarker(place);
+  });
+}
+
 
 // Knockout.js ViewModel
 var AppViewModel = function() {
   var self = this;
   self.currentLocation = ko.observable(sfLocation);
   self.currentMarker = ko.observable();
+  self.markers = ko.observableArray();
 
   self.setCurrentMarker = function(marker) {
     console.log('in set currentMarker, name: ' + marker.name + ' @: ' + marker.vicinity);
@@ -68,13 +79,11 @@ var AppViewModel = function() {
       address: marker.vicinity
     });
   };
-
-
 }
 
 
 
-//Activate knockout.js
+//Activating knockout.js
 myViewModel = new AppViewModel();
 ko.applyBindings(myViewModel);
 
